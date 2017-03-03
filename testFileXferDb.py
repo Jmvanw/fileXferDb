a4893da7d2a211a6473928c6fdd27ef9fdb9788e
jmvan <jmvanw@gmail.com>
2017-03-02 16:51:29 -0800

    FileXferDB
    Moves Files from Source to Destination with a GUI and uses a database to
    keep track of the previous transfer

--- testFileXferDb.py
+++ testFileXferDb.py
@@ -5,24 +5,10 @@ sqlite_file = 'fileXferTimeData.sqlite'    # name of the sqlite database file
 table_name = 'date_time_file_transfer'   # name of the table to be created
 index_name = 'unique_index' # index names
 main_column = 'last_check_column' # name of the PRIMARY KEY column
-#new_column1 = 'unique_names'  # name of the new column
-#new_column2 = 'my_3nd_column'  # name of the new column
 column_type = 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB, (TEXT REAL or INTEGER for Date and Time)
 default_val = '19:31:47.282902' # a default value for the new column rows
 
-####datetime('now','localtime');
 
-##conn = sqlite3.connect(sqlite_file)
-##c = conn.cursor()
-
-##c.execute('CREATE TABLE {tn} ({ix} INTEGER PRIMARY KEY AUTOINCREMENT, {mc} {ct})'\
-##          .format(tn=table_name, ix=index_name, mc=main_column, ct=column_type));
-
-##
-##c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-##        .format(tn=table_name, cn=new_column1, ct=column_type))
-
-#now = datetime('now')
 
 def updateTime():
     conn = sqlite3.connect(sqlite_file)
@@ -41,9 +27,3 @@ def updateTime():
 
 updateTime()    
 
-
-
-
-##
-##conn.commit()
-##conn.close()
